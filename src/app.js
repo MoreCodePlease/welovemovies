b5994aee481b1d7c8e8967628dd0f071e00a0155
MoreCodePlease <139405132+MoreCodePlease@users.noreply.github.com>
2023-11-27 21:05:26 -0600

    base app routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
@@ -9,6 +11,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/movies",moviesRouter);
+app.use("/reviews",reviewsRouter);
+app.use("/theaters",theatersRouter)
 
 
 //routes
